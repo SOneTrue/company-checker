20e4612c5a1ab8dcea640ec9d1482f1dc2ac6ca6
sone <jaredmanfarrell@gmail.com>
2022-04-24 23:38:36 +0300

    Added a working response to the accept and reject buttons

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,7 +1,10 @@
+import types
+
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery, ContentType
 
+from tgbot.filters.button_filter import Button
 from tgbot.keyboards.inline import accept_keyboard
 from tgbot.misc.states import Name
 from tgbot.models.users import User
@@ -19,6 +22,11 @@ async def state_name(message: Message, state: FSMContext):
     await message.answer(f'Вы ввели {answer}')
 
 
+async def inline_mode(call: CallbackQuery):
+    await call.message.send_copy(call.message.from_user.id)
+
+
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"])
     dp.register_message_handler(state_name, state=Name.send_name)
+    dp.register_callback_query_handler(inline_mode, Button('accept'))
