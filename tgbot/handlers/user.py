08744d220d5d3a26420c42347ea3cf78f4ea2aea
sone <jaredmanfarrell@gmail.com>
2022-04-26 22:54:44 +0300

    add update user info -> real_name

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -2,10 +2,18 @@ from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, CallbackQuery
 
+from tgbot.config import load_config
 from tgbot.filters.button_filter import Button
 from tgbot.keyboards.inline import accept_keyboard
 from tgbot.misc.states import Name
 from tgbot.models.users import User
+from tgbot.services.database import create_db_session
+
+
+async def start_db():
+    config = load_config()
+    session_maker = await create_db_session(config)
+    return session_maker
 
 
 async def user_start(message: Message, user: User):
@@ -13,15 +21,18 @@ async def user_start(message: Message, user: User):
     await Name.send_name.set()
 
 
-async def state_name(message: Message, state: FSMContext):
+async def state_name(message: Message, state: FSMContext, user: User):
     answer = message.text
     await message.send_copy(-1001709967985, reply_markup=accept_keyboard)
     await state.finish()
+    await user.update_user(session_maker=await start_db(), updated_fields={'real_name': answer})
     await message.answer(f'Вы ввели {answer}')
 
-#TODO доделать ответ пользователю после нажатия кнопки, одобрено или нет.
+
+# TODO доделать ответ пользователю после нажатия кнопки, одобрено или нет.
 async def inline_accept(call: CallbackQuery):
     await call.message.answer('Ok')
+    await call.message.forward(1462906954)
 
 
 async def inline_cancel(call: CallbackQuery):
