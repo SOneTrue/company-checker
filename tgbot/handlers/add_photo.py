26be895aaf596a65d28a13fc1432f3ce19f1ce9c
WORK_user <www.pavel_ru.ru@mail.ru>
2022-06-06 21:30:12 +0300

    Обновлены стейты, первый рабочий код с рабочими стейтами и типами данных.

---
+++ tgbot/handlers/add_photo.py
@@ -0,0 +1,13 @@
+from aiogram import Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message, ContentType
+from tgbot.misc.states import Name
+
+
+async def user_photo_fuel(message: Message, state: FSMContext):
+    await message.answer(f'Готово')
+    await state.finish()
+
+
+def register_photo(dp: Dispatcher):
+    dp.register_message_handler(user_photo_fuel, state=Name.send_photo_fuel, content_types=types.ContentTypes.PHOTO)
