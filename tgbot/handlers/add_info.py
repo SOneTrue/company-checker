740252e58474cb10b01a9a9409a4691041a5763f
WORK_user <www.pavel_ru.ru@mail.ru>
2022-06-07 19:53:11 +0300

    Доработана отправка фото и текста на заезд.

--- tgbot/handlers/add_info.py
+++ tgbot/handlers/add_info.py
@@ -1,11 +1,13 @@
 from aiogram import Dispatcher
-# from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 from tgbot.misc.states import Name
 from tgbot.models.users import get_info_user
 
 
-async def user_get_info(message: Message):
+# Выезд.
+
+async def user_info(message: Message):
     data = await get_info_user(telegram_id=message.from_user.id)
     real_name = ''.join(data)
     await message.answer(f"Ваше имя: {real_name}")
@@ -28,11 +30,38 @@ async def user_road_list(message: Message):
 async def user_odometer(message: Message):
     await message.answer(f'Одометр готов \n'
                          f'Отравьте фото датчика топлива.')
-    await Name.send_photo_fuel.set()
+    await Name.send_fuel.set()
+
+
+# Заезд.
+
+
+async def user_info_back(message: Message):
+    data = await get_info_user(telegram_id=message.from_user.id)
+    real_name = ''.join(data)
+    await message.answer(f"Ваше имя: {real_name}\n"
+                         f"Ваш гос. знак - .\n"
+                         f"Введите одометр на заезд")
+    await Name.send_odometer_back.set()
+
+
+async def user_odometer_back(message: Message):
+    await message.answer(f"Одометр введён.\n"
+                         f"Введите одометр на заезд")
+    await Name.send_litre_back.set()
+
+
+async def user_litre_back(message: Message):
+    await message.answer(f"Литры на заезд введены.\n"
+                         f"Отправьте фото датчика топлива на заезд!")
+    await Name.send_fuel_back.set()
 
 
 def register_info(dp: Dispatcher):
-    dp.register_message_handler(user_get_info, commands=["open"])
+    dp.register_message_handler(user_info, commands=["open"])
     dp.register_message_handler(user_number, state=Name.send_number_auto)
     dp.register_message_handler(user_road_list, state=Name.send_road_list)
     dp.register_message_handler(user_odometer, state=Name.send_odometer)
+    dp.register_message_handler(user_info_back, commands=["close"])
+    dp.register_message_handler(user_odometer_back, state=Name.send_odometer_back)
+    dp.register_message_handler(user_litre_back, state=Name.send_litre_back)
