20b658efc997f8f6c397ad4a44abbaf33165aa55
WORK_user <www.pavel_ru.ru@mail.ru>
2022-06-28 22:44:08 +0300

    Добавлены кнопки о подтверждении или комментировании чек листов.

--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -7,3 +7,11 @@ start_exit = ListOfButtons(text=['Начать раб. день'],
 start_close = ListOfButtons(text=['Закончить раб. день'],
                             callback=['close'],
                             align=[1]).inline_keyboard
+
+answer_q1 = ListOfButtons(text=['Верно', 'Комментарий'],
+                          callback=['good_1', 'comment_1'],
+                          align=[2]).inline_keyboard
+
+answer_q2 = ListOfButtons(text=['Верно', 'Комментарий'],
+                          callback=['good_2', 'comment_2'],
+                          align=[2]).inline_keyboard
